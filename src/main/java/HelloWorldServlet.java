1419fa375fec322b2bcca0095e50cdd0d67d38ef
James Muralles <james.muralles23@gmail.com>
2020-04-20 17:06:51 -0500

    didnt finish bonus'

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,45 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    private int views;
+
+    public void init() {
+        // Reset hit counter.
+        views = 0;
     }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, World!</h1>");
+
+        views++;
+        String title = "Page views";
+        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor = \"#f0f0f0\">\n" +
+                "<h1 align = \"center\">" + title + "</h1>\n" +
+                "<h2 align = \"center\">" + views + "</h2>\n" +
+                "</body> </html>"
+      );
+    }
+
+//        if (request.getParameterMap().containsKey("name")) {
+//            String username = request.getParameter("name");
+//        }
+
+
 }
